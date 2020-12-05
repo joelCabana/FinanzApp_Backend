e9d0c9e814fe151c23a18c5cca6f9b08b28efcbf
joelCabana <joelxcabana@gmail.com>
2020-12-05 17:28:08 -0300

    Fix: Add operations in user

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,8 @@ const userSchema = new Schema({
     first_name:{type:String, required:true},
     last_name:{type:String, required:true},
     email:{type:String,required:true},
-    password:{type:String,required:true}
+    password:{type:String,required:true},
+    operation:[{type:Operation.Schema, required:false}]
 })
 
 module.exports = model('User',userSchema);
