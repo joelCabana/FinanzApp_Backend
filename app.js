d83c43414b37be921d977ad46f7c864d72c37acd
joelCabana <joelxcabana@gmail.com>
2020-12-05 17:08:06 -0300

    Feat: Add index.js for run server

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 //execute express
 const app = express();
 //configuration
-app.set('port',process.env.PORT || 300);
+app.set('port',process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
---
+++ index.js
@@ -0,0 +1,6 @@
+require('./database');
+
+const app = require('./app');
+
+app.listen(app.get('port'));
+console.log('server on port ',process.env.PORT || app.get('port'));
