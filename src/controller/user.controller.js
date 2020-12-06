b79802c16f007d501ffbd3acfd29a58cfd82b140
joelCabana <joelxcabana@gmail.com>
2020-12-05 23:37:27 -0300

    Feat: add populate in user.operation for show category

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 const userCtrl = {};
 
 userCtrl.getUsers = async(req,res)=>{
-    const users = await User.find();
+    const users = await User.find().populate("operation.category");
     res.json(users);
 }
 
